55822f413907c207b14c1975fa943d662153befc
Dhruv Panchal <dhruv050992@hotmail.com>
2021-08-02 15:03:47 +0530

    Developed Basket and Catalog API

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get;  }
+    }
+}
