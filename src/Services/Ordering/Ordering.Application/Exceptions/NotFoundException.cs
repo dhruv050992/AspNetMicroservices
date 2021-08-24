a01dad4eb7b9c3778c5ff0fd08e765df8f5336f6
Dhruv Panchal <dhruv050992@hotmail.com>
2021-08-25 01:05:27 +0530

    Ordering API Changes till service registration in Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
